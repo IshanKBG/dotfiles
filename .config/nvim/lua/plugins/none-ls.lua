c3339fe1094ef6eeba98629ae032bca01a6a459d
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-04-14 12:36:28 +0530

    Add gitsigns

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,19 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.black,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.eslint_d,
-      },
-    })
+			},
+		})
 
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+	end,
 }
