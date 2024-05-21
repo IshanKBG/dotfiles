e3492dc527bb28aa4c54b74a79b3703ca1380a9b
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-05-21 10:20:08 +0530

    feat: refactor options and keymaps into separate files

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,21 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.markdown_fenced_languages = {
+	"ts=typescript",
+}
+vim.g.mapleader = " "
+vim.g.background = "light"
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.wo.number = true
+vim.keymap.set("n", "<M-,>", "<c-w>5<")
+vim.keymap.set("n", "<M-.>", "<c-w>5>")
+vim.keymap.set("n", "<M-t>", "<C-W>+")
+vim.keymap.set("n", "<M-s>", "<C-W>-")
+
